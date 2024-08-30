8f89d6bd4c420bb6a624cf87ab9c95e5f8ae2444
AdiSalL <adiefsal@gmail.com>
2024-08-30 08:28:53 +0700

    Error Page

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -4,12 +4,26 @@ namespace App\Http\Controllers;
 
 use Dotenv\Exception\ValidationException;
 use GuzzleHttp\Psr7\Response;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response as HttpResponse;
 use Illuminate\Support\Facades\Response as FacadesResponse;
+use Illuminate\Support\Facades\Validator;
 
 class FormController extends Controller
 {
+    public function form() {
+        return view('form');
+    }
+
+    public function submitForm(Request $request): HttpResponse {
+        $data = $request->validate([
+            "username" => "required",
+            "password" => "required",
+        ]);
+
+        return response("OK", HttpResponse::HTTP_BAD_REQUEST);
+    }
     //
     public function login(Request $request): HttpResponse {
         try {
@@ -22,4 +36,6 @@ class FormController extends Controller
             return response()->json(['error' => $exception->getMessage()], HttpResponse::HTTP_BAD_REQUEST);
         }
     }
+
+   
 }
